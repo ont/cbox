cd8c47278347e972048b62dd76f9531932c0eb12
ont.rif <ont.rif@gmail.com>
2009-11-27 19:15:07 +0700

    (+) new ability of reper to __mul__ with tuple
    After multiplication with tuple of three values a
    new reper is produced ( similar to frac2dec aspect )
    but more beautiful.

--- libc/reper.py
+++ libc/reper.py
@@ -11,6 +11,11 @@ class Reper( object ):
     def __len__( self ):
         return 3
 
+    def __mul__( self, obj ):
+        return self.v1 * obj[0] +\
+               self.v2 * obj[1] +\
+               self.v3 * obj[2]
+
     def __repr__( self ):
         return 'reper( %s,%s,%s )' % ( self.v1, self.v2, self.v3 )
 
