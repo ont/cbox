6ce120af638d59c2fe5907c7563ecf4d315216e5
ont.rif <ont.rif@gmail.com>
2010-03-17 15:33:24 +0700

    (m) name of space group in dump

--- main/sym2cpp.py
+++ main/sym2cpp.py
@@ -15,8 +15,8 @@ f = open( sys.argv[ 1 ], 'w' )
 
 N = sum( map( lambda n: len( SpGrp.subs( n ) ), xrange( 1, 231 ) ) )
 f.write( pack( 'i', N ) )
-f.close()
-exit( 0 )
+#f.close()
+#exit( 0 )
 
 for n in xrange( 1, 231 ):
     for ns in SpGrp.subs( n ):
@@ -24,8 +24,12 @@ for n in xrange( 1, 231 ):
         ops = s.full()
         print 'saving -->', s, len( ops )
 
-        f.write( pack( 'i', n * 100 + ns ) )  ## write seed
-        f.write( pack( 'i', len( ops ) ) )    ## count of ops
+        symb = s.mydata['symb'].strip()
+
+        ## ????  why i can't use 'i30si' ????
+        f.write( pack( 'i'  , n * 100 + ns ) )   ## write seed, symbol, count of ops
+        f.write( pack( '30s', symb         ) )
+        f.write( pack( 'i'  , len( ops )   ) )
         for m,v in ops:
             f.write( pack( 'ddddddddd', m[ 0 ][ 0 ], m[ 0 ][ 1 ], m[ 0 ][ 2 ], m[ 1 ][ 0 ], m[ 1 ][ 1 ], m[ 1 ][ 2 ], m[ 2 ][ 0 ], m[ 2 ][ 1 ], m[ 2 ][ 2 ] ) )
             f.write( pack( 'ddd', v.x, v.y, v.z ) )
