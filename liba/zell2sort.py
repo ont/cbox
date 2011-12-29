8feca237e00484fe616ec1b0e9e834c56a065977
ont.rif <ont.rif@gmail.com>
2011-12-29 20:46:45 +0700

    (f) removing +sort & fix sort detection
    Completely removing additional sorts!! Now we iterate over _all_
    possible symbols.

--- liba/zell2sort.py
+++ liba/zell2sort.py
@@ -1,3 +1,4 @@
+import zell
 from sort import Sort
 
 import zell_norm    ## aspect
@@ -8,27 +9,22 @@ def to_sort( self ):
                 ("K1"   , (1, 1, 1, 1, 1, 1)),
                 ("K3"   , (1, 0, 1, 1, 0, 1)),
                 ("K5"   , (0, 0, 0, 1, 1, 1)),
-                ("H4"   , (0, 1, 0, 1, 2, 1)),
 
+                ("H4"   , (0, 1, 0, 1, 2, 1)),
                 ("Q1"   , (1, 2, 1, 1, 2, 1)),
                 ("Q2"   , (1, 0, 1, 1, 2, 1)),
                 ("Q5"   , (0, 0, 0, 1, 2, 1)),
                 ("R1"   , (1, 1, 1, 2, 2, 2)),
                 ("R3"   , (1, 0, 1, 1, 0, 2)),
                 ("O3"   , (1, 0, 2, 1, 0, 2)),
-                ("+O3"  , (1, 0, 2, 2, 0, 1)),
 
                 ("O4"   , (0, 2, 0, 1, 3, 1)),
-                ("+O4"  , (0, 1, 0, 1, 2, 3)),
                 ("O11"  , (1, 2, 1, 1, 3, 1)),
                 ("O12"  , (1, 3, 2, 1, 3, 2)),
                 ("O2"   , (1, 0, 2, 2, 3, 1)),
                 ("O5"   , (0, 0, 0, 1, 2, 3)),
-                ("+O5"  , (0, 0, 1, 0, 2, 3)),
                 ("M22"  , (1, 0, 2, 1, 3, 2)),
-                ("+M22" , (2, 0, 2, 1, 3, 1)),
                 ("M3"   , (1, 0, 2, 1, 0, 3)),
-                ("+M3"  , (2, 0, 1, 1, 0, 3)),
 
                 ("M11"  , (1, 3, 2, 1, 4, 2)),
                 ("M12"  , (1, 3, 1, 2, 4, 2)),
@@ -44,33 +40,51 @@ def to_sort( self ):
     def test( c, z ):
         g = {}
         for i in xrange( 6 ):
+            n, v = c[ i ], abs( z[ i ] )
+            g[ n ] = max( v, g.get( n, v ) )  ## store in group max possible abs value
+
+        for i in xrange( 6 ):
             n = c[ i ]
-            if ( n == 0 and abs( z[ i ] ) > 0.001 ) or\
-               ( n != 0 and abs( z[ i ] ) < 0.001 ):     ## zeroes only on zeores
+            if ( n == 0 and abs( z[ i ] ) > 0.01 ) or\
+               ( n != 0 and abs( z[ i ] ) < 0.01 ):     ## zeroes only on zeores
                 return False
-            else:
-                v = g.get( n, None )
-                if v:                                    ## test with group
-                    if abs( z[ i ] - v ) > 0.001:
-                        return False
-                else:
-                    g[ n ] = z[ i ]                      ## new group
+            elif n != 0 and ( g[ n ] - abs( z[ i ] ) ) / g[ n ] > 0.01:
+                return False
 
         return True
 
 
-    z = self.norm()
-    for c in cond:
-        for p in xrange( 24 ):
-            zr = z.rotate( p )
+    ## pairs to swap for each edge
+    ps = { 0: [ (2,4), (1,5) ],
+           1: [ (0,5), (2,3) ],
+           2: [ (1,3), (0,4) ],
+           3: [ (1,2), (4,5) ],
+           4: [ (0,2), (3,5) ],
+           5: [ (0,1), (3,4) ] }
 
-            if test( c[ 1 ], zr ):
-                #print '-->', zr
-                #print '-->', c[ 1 ]
+    z = self.norm()
+
+    ## generate all possible zellings for testing
+    zs = set([])
+    zs.update([ z.rotate( p ) for p in xrange( 24 ) ])  ## add all rotations
+    for i, x in enumerate( z ):
+        if x == 0:                                      ## hey! we can generate more symbols!
+            p1, p2 = ps[ i ]                            ## take pairs for swapping
+            for p in (p1, p2):                          ## for each pair...
+                tmp = list( z )
+                a,b = tmp[ p[0] ], tmp[ p[1] ]          ## current values
+                tmp[ p[0] ], tmp[ p[1] ] = b,a          ## swap...
+                zn = zell.Zell( *tmp )                  ## new zell symbol
+                zs.update([ zn.rotate( p ) for p in xrange( 24 ) ])  ## add all rotations
+
+    for c in cond:
+        for z in zs:
+            if test( c[ 1 ], z ):
                 return Sort( c[ 0 ] )
 
+    raise "Whaaaat!?  we can't find sort for %s ???" % self
 
-import zell
+
 zell.Zell.to_sort = to_sort
 zell.Zell.cond =  {
             "K1"   : (1, 1, 1, 1, 1, 1),
--- libc/zell.py
+++ libc/zell.py
@@ -15,6 +15,15 @@ class Zell( object ):
         t = ( self.g, self.h, self.k, self.l, self.m, self.n )
         return t[ idx ]
 
+    def __hash__( self ):
+        return 1 ## zell as a vec is not hashable
+                 ## see stackoverflow site for this reason int( sum( self ) * 10 )
+
+
+    def __eq__( self, other ):
+        tmp = map( lambda t: abs( t[ 0 ] - t[ 1 ] ) < 0.001, zip( self, other ) )  ## how different each pair ?
+        return filter( lambda x: not x, tmp ) == []                                ## does here exists too different ?
+
 
     def __repr__( self ):
         return "zell( %s %s %s %s %s %s )" % ( self.g, self.h, self.k, self.l, self.m, self.n )
