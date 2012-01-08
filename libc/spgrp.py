6a620057220e4ac084c040249cd6069201a5dd65
ont.rif <ont.rif@gmail.com>
2012-01-08 14:04:19 +0700

    (+) aliases for ICSD tetragonal groups

--- libc/spgrp.py
+++ libc/spgrp.py
@@ -12,8 +12,66 @@ class SpGrp( object ):
                   ##                         wyck =  wyckoff positions
                   ##                         gens =  generators ( type, (x,y,z) )
 
-    syns = { (139,1): ['I 4/m m m'],
-             (140,1): ['I 4/m c m'],
+    ## below are some aliases for ICSD crystallographic database
+    syns = { (76 ,1): ['P 41'],
+             (77 ,1): ['P 42'],
+             (78 ,1): ['P 43'],
+             (80 ,1): ['I 41'],
+             (84 ,1): ['P 42/m'],
+             (85 ,1): ['P 4/n S'],
+             (85 ,2): ['P 4/n Z'],
+             (86 ,1): ['P 42/n S'],
+             (86 ,2): ['P 42/n Z'],
+             (87 ,1): ['F 4/m'],   ## TODO: it is bug in ICSD! (must be "I 4/m", check bilbao crystallographic server)
+             (88 ,1): ['I 41/a S'],
+             (88 ,2): ['I 41/a Z', 'I 41/a Z1'],
+             (90 ,1): ['P 4 21 2', 'C 4 2 21' ],  ## TODO: what is "C 4 2 21"
+             (91 ,1): ['P 41 2 2'],
+             (92 ,1): ['P 41 21 2'],
+             (94 ,1): ['P 42 21 2'],
+             (95 ,1): ['P 43 2 2'],
+             (96 ,1): ['P 43 21 2'],
+             (98 ,1): ['I 41 2 2'],
+             (101,1): ['P 42 c m'],
+             (102,1): ['P 42 n m'],
+             (105,1): ['P 42 m c'],
+             (106,1): ['P 42 b c'],
+             (109,1): ['I 41 m d'],
+             (110,1): ['I 41 c d'],
+             (113,1): ['P -4 21 m', 'C -4 m 21'],  ## TODO: what is "C -4 m 21" ?
+             (114,1): ['P -4 21 c'],
+             (117,1): ['C -4 2 b'],                ## TODO: what is "C -4 2 b"
+             (123,1): ['P 4/m m m'],
+             (122,1): ['F -4 d 2'],                ## TODO: what is "F -4 d 2"
+             (124,1): ['P 4/m c c', 'C 4/m c c'],  ## TODO: what is "C 4/m c c"
+             (125,1): ['P 4/n b m S'],
+             (125,2): ['P 4/n b m Z'],
+             (126,1): ['P 4/n n c S'],
+             (126,2): ['P 4/n n c Z'],
+             (127,1): ['P 4/m b m'],
+             (128,1): ['P 4/m n c'],
+             (129,1): ['P 4/n m m S', 'P 4/n m m'],
+             (129,2): ['P 4/n m m Z'],
+             (130,1): ['P 4/n c c S'],
+             (130,2): ['P 4/n c c Z'],
+             (131,1): ['P 42/m m c'],
+             (132,1): ['P 42/m c m'],
+             (133,1): ['P 42/n b c S'],
+             (133,2): ['P 42/n b c Z'],
+             (134,1): ['P 42/n n m S'],
+             (134,2): ['P 42/n n m Z'],
+             (135,1): ['P 42/m b c'],
+             (136,1): ['P 42/m n m'],
+             (137,1): ['P 42/n m c S'],
+             (137,2): ['P 42/n m c Z'],
+             (138,1): ['P 42/n c m S'],
+             (138,2): ['P 42/n c m Z'],
+             (139,1): ['I 4/m m m', 'F 4/m m m'],        ## TODO: what is "F 4/m m m"
+             (140,1): ['I 4/m c m', 'F 4/m m c'],        ## TODO: what is "F 4/m m c"
+             (141,1): ['I 41/a m d S'],
+             (141,2): ['I 41/a m d Z', 'F 41/d d m Z'],  ## TODO: what is "F 41/d d m Z"
+             (142,1): ['I 41/a c d S', 'F 41/a d c'],    ## TODO: what is "F 41/a d c"
+             (142,2): ['I 41/a c d Z'],
              (167,2): ['R -3 c R' ],
              (198,1): ['P 21 3'   ],
              (199,1): ['I 21 3'   ],
