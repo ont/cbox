4be9000bda5d0f9736b65b40d0034845e97b553e
ont.rif <ont.rif@gmail.com>
2012-03-19 21:11:09 +0700

    (+) uctool.py minimize and ipe abilities

--- main/uctool.py
+++ main/uctool.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-import sys
+import os, sys
 import argparse
 sys.path.extend( [ '../lib', '../liba', '../libc' ] )
 
@@ -17,6 +17,8 @@ g = p.add_argument_group( 'info' )
 g.add_argument( '-i', '--info',  action = 'store_true', help = 'get full info about unit cell' )
 g.add_argument( '-s', '--sort',  action = 'store_true', help = 'get info about sort of unit cell reper' )
 g.add_argument( '-z', '--zell',  action = 'store_true', help = 'get zelling symbol for reper of unit cell' )
+g = p.add_argument_group( 'drawing' )
+g.add_argument( '-p', '--ipe',   action = 'store_true', help = 'add "save to ipe" button in 3D view' )
 g = g.add_mutually_exclusive_group()
 g.add_argument( '-v', '--voron', action = 'store_true', help = 'build & draw Voronoi cell' )
 g.add_argument( '-d', '--draw',  action = 'store_true', help = 'draw usual unit cell' )
@@ -39,7 +41,8 @@ def zell():
     import zell_norm
     print 'zell:', u.rep.to_zell().norm()
 
-def voron():
+
+def voron( to_ipe = False ):
     from draw_gl import drawgl
     from voron import Voron
     import vec_gl
@@ -47,17 +50,54 @@ def voron():
     import voron_gl
     import voron_inout
 
-    vo = Voron( *u.rep )
-    drawgl( vo )
-    u.to_decart()
-    for n, vs in u*1:                 ## for each atoms in extended unitcell
-        for v in vo.has( vs ):        ## only draw atoms which are in Voron cell
+    def draw_with( eng ):
+        eng.clear()
+        vo = Voron( *u.rep )
+        eng( vo )
+        u.to_decart()
+        for n, vs in u*1:                 ## for each atoms in extended unitcell
             r, col = UCell.data[ n ]
             col = [ x/255.0 for x in col ]
-            drawgl( v, r = r, color = col )
+
+            for v in vo.touch( vs ):
+                eng( v, r = r, color = col, invis = False )
+                eng( n, pos = v )
+
+            for v in vo.has( vs ):        ## only draw atoms which are in Voron cell
+                if not vo.touch( v ):
+                    eng( v, r = r, color = col, invis = False )
+                    eng( n, pos = v )
+
+
+    def save2ipe( *args ):
+        from draw_ipe import drawipe
+        i = 0
+        fname = '/tmp/out_%s.ipe'
+        while os.path.exists( fname % i ):
+            i += 1
+        fname = fname % i
+        print 'saving to %s...' % fname
+        drawipe.setup_drawgl( drawgl )
+        draw_with( drawipe )
+        drawipe.save( fname )
+
+    def minimize( *args ):
+        global u
+        u = u.to_min()
+        draw_with( drawgl )
+
+    draw_with( drawgl )
+
+    if to_ipe:
+        drawgl.button( 'save to ipe', save2ipe )
+
+    if args.min:
+        drawgl.button( 'minimize', minimize )
+
     drawgl.start()
 
-def draw():
+
+def draw( to_ipe = False ):
     from draw_gl import drawgl
     import vec_gl
     import ucell_gl
@@ -76,7 +116,8 @@ if args.spgrp:
 
 if args.min:
     import ucell_min
-    u = u.to_min()
+    if not args.voron:
+        u = u.to_min()  ## do minimization immidiately
 
 if args.info:
     print u
@@ -92,6 +133,6 @@ if args.zell:
     zell()
 
 if args.voron:
-    voron()
+    voron( args.ipe )
 elif args.draw:
-    draw()
+    draw( args.ipe )
